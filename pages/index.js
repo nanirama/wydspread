f6763f53e7928e3b89ea0763232132c968f04ced
nanirama <nani.rama1982@gmail.com>
2021-10-07 08:26:32 +0530

    changes 1

--- pages/index.js
+++ pages/index.js
@@ -65,8 +65,8 @@ return (
       <div className="container-fluid">
          <div className="d-flex justify-content-between mb-2 ps-xl-5 mt-lg-0 mt-md-3 mb-lg-2 mb-md-2 mb-5 px-lg-1">
             <div className="logo ps-lg-3"><a href="#"><img src="images/logo.png"/></a></div>
-            <button type="button" className="btn text-white rounded-pill px-4 me-lg-2 mt-lg-1">Register <span className="arr_icon">&nbsp;</span>
-            </button>
+            {/* <button type="button" className="btn text-white rounded-pill px-4 me-lg-2 mt-lg-1">Register <span className="arr_icon">&nbsp;</span>
+            </button> */}
          </div>
          <div className="row d-flex justify-content-between align-items-center text-lg-start text-center ps-xl-5 mb-lg-5">
             <div className="col-xl-6 col-lg-7 col-xs-12">
@@ -74,7 +74,7 @@ return (
                   <h2 className="mb-3 mt-lg-3 pt-lg-2 ps-lg-1">Build and scale<br/>
                      <span className="text-white">your online presence <br/>with zero effort</span>
                   </h2>
-                  <p className="mb-3 ps-lg-1 mt-3 pt-1">Introducing you own dedicated AI outreach platform that <br/>will allow you to manage/scale your online presence.</p>
+                  <p className="mb-3 ps-lg-1 mt-3 pt-1">Introducing your own dedicated AI outreach platform that <br/>will allow you to manage/scale your online presence.</p>
                   <h4 className="text-white mb-3 ps-lg-1 pt-1 px-lg-0 px-md-0 px-sm-0 px-4">No need of a SEO specialist or a <br/>marketing manager.</h4>
                   <p className="ps-lg-1 mt-3 pt-1">Join for free early access</p>
                      {isSuccess ? (
@@ -92,9 +92,9 @@ return (
                               className="text-white rounded-pill me-lg-2 mb-lg-0 mb-mb-2 mb-3 px-4 py-3 bg-transparent"
                               onChange = {inputChangeHandler}/>
                               
-                              <div className="signup_button rounded-pill border-0 py-3 mt-md-1 mt-lg-0">
-                              <button type="submit" class="bg-transparent border-0 text-white">
-                                 Early Access <span className="arr_icon">&nbsp;</span>                     
+                              <div className="signup_button rounded-pill border-0 py-3 mt-md-1 mt-lg-0 d-flex justify-content-center">
+                              <button type="submit" class="bg-transparent border-0 text-white d-flex justify-content-between align-items-center">
+                                 <span>Early Access</span> <span className="arr_icon">&nbsp;</span>                     
                               </button>       
                               </div>  
                                                    
@@ -136,13 +136,14 @@ return (
          <div className="footer pt-4 pb-lg-2 pb-md-2 pb-0 ps-xl-5 pt-lg-2 pt-md-5 mt-lg-0 mt-md-5">
            
             <p className="mb-0 text-center text-lg-start ps-xl-4">
-               All Rights Reserved 2021® &nbsp; &nbsp;| 
+               All Rights Reserved 2021® &nbsp; &nbsp;
+               {/* | 
                <a href="#" className="ps-2 text-white text-decoration-none">
                   <svg class="svg-icon" viewBox="0 0 20 20"> 
                      <path fill="none" d="M18.258,3.266c-0.693,0.405-1.46,0.698-2.277,0.857c-0.653-0.686-1.586-1.115-2.618-1.115c-1.98,0-3.586,1.581-3.586,3.53c0,0.276,0.031,0.545,0.092,0.805C6.888,7.195,4.245,5.79,2.476,3.654C2.167,4.176,1.99,4.781,1.99,5.429c0,1.224,0.633,2.305,1.596,2.938C2.999,8.349,2.445,8.19,1.961,7.925C1.96,7.94,1.96,7.954,1.96,7.97c0,1.71,1.237,3.138,2.877,3.462c-0.301,0.08-0.617,0.123-0.945,0.123c-0.23,0-0.456-0.021-0.674-0.062c0.456,1.402,1.781,2.422,3.35,2.451c-1.228,0.947-2.773,1.512-4.454,1.512c-0.291,0-0.575-0.016-0.855-0.049c1.588,1,3.473,1.586,5.498,1.586c6.598,0,10.205-5.379,10.205-10.045c0-0.153-0.003-0.305-0.01-0.456c0.7-0.499,1.308-1.12,1.789-1.827c-0.644,0.28-1.334,0.469-2.06,0.555C17.422,4.782,17.99,4.091,18.258,3.266"></path>
                   </svg>
                   <span className="ps-2">Follow at Twitter For updates</span>
-               </a>
+               </a> */}
             </p>
          </div>
       </div>
